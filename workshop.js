e26285be29ca37d948fcddbff9bff8edd9273a9d
Nicolas Hoog <nicolas.hoog@gmail.com>
2017-04-12 21:37:54 +0000

    workshop done

--- workshop.js
+++ workshop.js
@@ -6,29 +6,49 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-    return request(/* ... */)
-        .then(
-            function(responce) {
-                // Parse as JSON
-                // Return object with lat and lng
+    return request('http://api.open-notify.org/iss-now.json')
+        .then(function(response) {
+            var data = JSON.parse(response); // Parse as JSON
+            var postion = {
+                lng : data.iss_position.longitude,
+                lat : data.iss_position.latitude
             }
-        )
+            return postion;
+            
+            // return data.iss_position;// Return object with lat and lng
+        })
 }
 
 function getAddressPosition(address) {
+    return request('https://maps.googleapis.com/maps/api/geocode/json?address=' + address)
+        .then(function(response) {
+            var data = JSON.parse(response);
+            
+            return data.results[0].geometry.location;
+        })
 
 }
 
 function getCurrentTemperatureAtPosition(position) {
+    return request('https://api.darksky.net/forecast/909a03baced3035eb51d7c806f00ba53/' + position.lat + ',' + position.lng)
+        .then(function(response) {
+            var data = JSON.parse(response);
+        
+            return data.currently.temperature;
+        })
 
 }
 
 function getCurrentTemperature(address) {
+    return getCurrentTemperatureAtPosition(getAddressPosition(address));
 
 }
 
 function getDistanceFromIss(address) {
-
+    return Promise.all([getAddressPosition(address), getIssPosition()])
+        .then(function(position) {
+            return getDistance(position[0], position[1]);
+        });
 }
 
 exports.getIssPosition = getIssPosition;
